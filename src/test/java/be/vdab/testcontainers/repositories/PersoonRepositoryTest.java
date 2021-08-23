a2fc6272829e6439d0d0b4c85a0ae982c4a6d47a
ibrahimakbiyik <ibrahim.akbiyik@vdabcampus.be>
2021-08-23 12:35:21 +0200

    1

---
+++ src/test/java/be/vdab/testcontainers/repositories/PersoonRepositoryTest.java
@@ -0,0 +1,48 @@
+package be.vdab.testcontainers.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJpaTest
+@Sql("/insertPersonen.sql")
+class PersoonRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private final PersoonRepository repository;
+
+    @Container
+    private static final MySQLContainer<?> mySQL = new MySQLContainer<>("mysql:latest")
+            .withDatabaseName("testcontainers")
+            .withUsername("cursist")
+            .withPassword("cursist");
+
+    PersoonRepositoryTest(PersoonRepository repository) {
+        this.repository = repository;
+    }
+
+    @DynamicPropertySource
+    private static void source(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQL::getJdbcUrl);
+    }
+
+    @Test
+    void erIs1Persoon() {
+        assertThat(repository.count()).isOne();
+    }
+
+    @Test
+    void dePersoonIsJoe() {
+        var eerstePersoon = repository.findAll().get(0);
+        assertThat(eerstePersoon.getVoornaam()).isEqualTo("Joe");
+    }
+
+
+}
